9863c8855d68d78cadd01285f9d1a55a4c724109
Lursy <115332598+Lursy@users.noreply.github.com>
2023-02-27 07:37:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 
 int main(void){
-        int primos[10000] = {2};
-        int length = 1;
+        int primos[15000] = {};
+        int length = 0, real = 0, maior = 0;
 
-        for(int i=3; i!=100000; i++){
+        for(int i=2; i!=1000000; i++){
                 int primo = 1;
                 for(int c=0; c!=length; c++){
                         if(i%primos[c] == 0){
+                                if (primos[c] > maior){
+                                        maior = primos[c];
+                                }
                                 primo = 0;
                                 break;
                         }
                 }
 
-                switch(primo){
-                        case 1:
+                if (primo == 1){
+                        if (i < 997){
                                 primos[length] = i;
-                                printf("%d\n", i);
                                 length++;
-                                break;
+                        }
+                        printf("[%d]  ", i);
+                        real++;
                 }
         }
-        printf("Lista: %d", length);
+        printf("Lista: %d, %d, %d, _%d_", real, primos[length-1], maior, length);
 }
